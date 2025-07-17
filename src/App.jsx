508f60f626c94aefc5f93f18b31045ae0f9c7160
urvashiyadav <urvashiy678@gmail.com>
2025-07-17 16:15:55 +0530

    hero done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { useState } from 'react'
 import { ScrollTrigger, SplitText } from 'gsap/all'
 import gsap from 'gsap';
 import './App.css'
 import Navbar from './components/navbar/navbar';
+import HeroSection from './components/hero/HeroSection';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -10,8 +10,10 @@ function App() {
 
 
   return (
-    <main>
+    <main className='w-[100%]'>
       <Navbar />
+      <HeroSection />
+      <div className="h-dvh bg-black"></div>
     </main>
   )
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,32 +1,50 @@
-import React from 'react'
-import { navLinks } from '../../contants'
+import React from 'react';
+import { navLinks } from '../../contants';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
 
 const Navbar = () => {
 
+  useGSAP(() => {
+    const navTween = gsap.timeline({
+      scrollTrigger: {
+        trigger: 'nav',
+        start: "bottom top"
+      }
+    });
 
-  return (
-    <header>
-      <nav>
-        <div className="logo">
-          Velvet Pour
-        </div>
-        <div className="links">
-          {
-            navLinks.map(link => {
-              return (
-                <ul>
-                  <li key={link.id} >
-                    <a target='blank' href={`#${link.id}`}>{link.title}</a>
-                  </li>
-                </ul>
+    navTween.fromTo('nav', 
+        {
+        backgroundColor: 'transparent'
+      },
+      {
+         backgroundColor : '#00000050',
+         backgroundFilter : 'blur(10px)',
+         duration : 1,
+         ease : 'power1.inOut'
+      }
+      );
 
-              )
-            })
-          }
-        </div>
-      </nav>
-    </header>
-  )
-}
+}, [])
 
-export default Navbar
+return (
+  <nav>
+    <div>
+      <a href="#home" className="flex items-center gap-2">
+        <img src="/images/logo.png" alt="logo" />
+        <p>Velvet Pour</p>
+      </a>
+
+      <ul>
+        {navLinks.map((link) => (
+          <li key={link.id}>
+            <a href={`#${link.id}`}>{link.title}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  </nav>
+);
+};
+
+export default Navbar;
