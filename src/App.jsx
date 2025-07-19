648ab4aa1a2165963fecdc36e0fd1d43be33b609
urvashiyadav <urvashiy678@gmail.com>
2025-07-19 16:57:09 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { ScrollTrigger, SplitText } from 'gsap/all'
 import gsap from 'gsap';
 import './App.css'
-import Navbar from './components/navbar/navbar';
+import Navbar from "./components/navbar/Navbar"
 import HeroSection from './components/hero/HeroSection';
 import Cocktails from './components/cocktail/cocktails';
 
